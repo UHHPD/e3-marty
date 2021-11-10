177e6b4c363f998d08ea4cdfada19561c8e7c0c3
ukmartylee <92804063+ukmartylee@users.noreply.github.com>
2021-11-10 17:01:42 +0000

    Finished exercises

--- likelihood.cc
+++ likelihood.cc
@@ -1,20 +1,50 @@
 #include <iostream>
 #include <fstream>
 #include <vector>
-
+#include <cmath>
 
 double poisson(double mu, int k) {
-    return 0;
+  return pow(mu, k)*exp(-mu)/tgamma(k+1);
+}
+
+double prob(std::vector<int> daten, double mu) {
+  double i = 1;
+  for(int k : daten) {
+    i *= poisson(mu, k);
+  }
+  return i;
+}
+
+double probk(std::vector<int> daten) {
+  double i = 1;
+  for(int k : daten) {
+    double a = 1.0 * k;
+    i *= poisson(a, k);
+  }
+  return i;
 }
 
 int main() {
-    using namespace std;
+  using namespace std;
 
+  vector<int> daten;
+  ifstream fin("datensumme.txt");
+  int n_i;
+  for(int i = 0 ; i < 234 ; ++i) {
+    fin >> n_i;
+    daten.push_back(n_i);
+  }
+  
+  double mu;
+  double mu_lower = 0.0;
+  double mu_upper = 6.0;
+  double step_size = 0.05;
 
-    ifstream fin("datensumme.txt");
-    int n_i;
-    for(int i = 0 ; i < 234 ; ++i) {
-        fin >> n_i;
-    }
-    fin.close();
+  for(mu=mu_lower; mu<=mu_upper; mu+=step_size) {
+    double lambda;
+    lambda = prob(daten, mu)/probk(daten);
+    cout << (-2*log(lambda)-233)/sqrt(2*233) << endl;
+  }
+
+  fin.close();
 }
